8f5a5a7ab3642cd3411177cae5915ebc7fd0cecc
amaldo4 <amaldo4@gmail.com>
2021-04-23 17:07:09 -0500

    added FIFO algo

--- program2.cpp
+++ program2.cpp
@@ -13,32 +13,78 @@ Austin Maldonado
 #include <ctime>
 #include <time.h>
 
+
+
 using namespace std;
+const int total = 100;
+
+bool hitMiss(int, int[], int);
 int FIFOFaults(int[], int);
 
 int main(){
-    int refString[100];
+    int refString[total];
+    //int testString[] = {1,2,3,1,6,5,6,3,4,6,8,7,1,0,5,4,6,2,3,0,1};
     int j = 0;
+    int numFrames = 0;
+    int FIFOCount = 0;
+
+    cout << "How many frames: ";
+    cin >> numFrames;
+
+
     srand(time(NULL));
 
-    for(int i = 0; i < 100; i++){
-        //refString += to_string(i);
+    for(int i = 0; i < total; i++){
         j = rand()%50;
-        cout << "J value is: " << j << endl;
         refString[i] = j;
     }
 
-    // test to make sure thre were actually values in the array.
-    /*for(int i = 0; i < 100; i++)
+
+    for(int i = 0; i < total; i++)
     {
-        cout << refString[i] << " ";
-    }*/
-    
+        cout <<"Position " << i  <<": "<< refString[i] << endl;
+    }
+    cout << endl;
+    cout << endl;
 
-
-
+    FIFOCount = FIFOFaults(refString, numFrames);
+    //FIFOCount = FIFOFaults(testString, numFrames);
+    cout << "Number of FIFO Faults: " << FIFOCount << endl;
 }
 
-int FIFOFaults(int refString[], int frames){
+bool hitMiss(int lookingFor, int frameArray[], int numFrames){
+    bool test = false;
+    for(int i = 0; i < numFrames; i++){
+        if(frameArray[i] == lookingFor){
+            test = true;
+        }
+    }
+    return test;
+}
+
+int FIFOFaults(int refString[], int numFrames){
+    bool found;
+    int frameArray[numFrames];
+    int lookingFor = 0;
+    int front = 0;
+    int rear = -1;
+    int faults = 0;
     
+
+    for(int i = 0; i < total; i++){
+        lookingFor = refString[i];
+        //cout << "looking for: " << lookingFor << endl;
+        found = hitMiss(lookingFor, frameArray, numFrames);
+        if(found == true){
+        }
+
+        else{
+            //cout << "didnt find in cache" << endl;
+            faults++;
+            front = (front + 1) % numFrames;
+            rear = (rear + 1) % numFrames;
+            frameArray[front] = refString[i];
+        }
+    }
+    return faults;
 }
